bb0e8014bf16dabf8132a9eb4a6b1ca914d2b083
theo ferguson <fergy546@yahoo.com>
2023-03-08 16:58:08 +0000

    complete

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class MostCommonCharacter {
     /**
      * Find the most common character in str.
@@ -8,6 +8,22 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        Map<Character, Integer> counter = new HashMap<Character, Integer>();
+        counter.put(str.charAt(0), 1);
+
+        char mostCommon = str.charAt(0);
+
+        for (int i = 1; i < str.length(); i++) {
+            if (counter.get(str.charAt(i)) != null) {
+                counter.put(str.charAt(i), counter.get(str.charAt(i))+1);
+                if (counter.get(mostCommon) < counter.get(str.charAt(i))) {
+                    mostCommon = str.charAt(i);
+                }
+            } else {
+                counter.put(str.charAt(i), 1);
+            }
+        }
+
+        return mostCommon;
     }
 }
